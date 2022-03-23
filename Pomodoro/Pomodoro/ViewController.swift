5710b7938138c2d235a9e88a9ecd07ce37141488
ehvkddl <ehvkddl@naver.com>
2022-03-24 01:47:57 +0900

    타이머 기능

--- Pomodoro/Pomodoro/ViewController.swift
+++ Pomodoro/Pomodoro/ViewController.swift
@@ -6,14 +6,115 @@
 //
 
 import UIKit
+import AudioToolbox
+
+enum TimerStatus {
+    case start
+    case pause
+    case end
+}
 
 class ViewController: UIViewController {
 
+    @IBOutlet var timerLabel: UILabel!
+    @IBOutlet var progressView: UIProgressView!
+    @IBOutlet var datePicker: UIDatePicker!
+    
+    @IBOutlet var cancelButton: UIButton!
+    @IBOutlet var toggleButton: UIButton!
+    
+    //타이머에 설정된 시간을 초로 저장하는 프로퍼티
+    var duration = 60 //앱 실행하면 date picker가 기본적으로 1분으로 설정되어 있기 때문에 60으로 초기화
+    var timerStatus: TimerStatus = .end
+    var timer: DispatchSourceTimer?
+    var currentSeconds = 0 //현재 카운트다운되고 있는 시간을 초로 저장한 프로퍼티
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.configureToggleButton()
     }
 
-
+    func setTimerInfoViewVisible(isHidden: Bool) {
+        self.timerLabel.isHidden = isHidden
+        self.progressView.isHidden = isHidden
+    }
+    
+    func configureToggleButton() {
+        self.toggleButton.setTitle("시작", for: .normal)
+        self.toggleButton.setTitle("일시정지", for: .selected)
+    }
+    
+    func startTimer() {
+        if self.timer == nil {
+            self.timer = DispatchSource.makeTimerSource(flags: [], queue: .main)
+            //타이머가 돌 때마다 UI관련 작업을 해주어야 하기 때문에 큐를 메인으로 설정
+            self.timer?.schedule(deadline: .now(), repeating: 1)
+            self.timer?.setEventHandler(handler: { [weak self] in
+                guard let self = self else {return} //일시적으로 self가 strong reference가 되도록 만듦
+                self.currentSeconds -= 1
+                let hour = self.currentSeconds / 3600
+                let minutes = (self.currentSeconds % 3600) / 60
+                let seconds = (self.currentSeconds % 3600) % 60
+                self.timerLabel.text = String(format: "%02d:%02d:%02d", hour, minutes, seconds)
+                self.progressView.progress = Float(self.currentSeconds) / Float(self.duration)
+                debugPrint(self.progressView.progress)
+                
+                if self.currentSeconds <= 0 {
+                    //0보다 작거나 같다면 카운트다운 끝남
+                    //타이머가 종료
+                    self.stopTimer()
+                    AudioServicesPlaySystemSound(1005)
+                }
+            })
+            self.timer?.resume()
+        }
+    }
+    
+    func stopTimer() {
+        if self.timerStatus == .pause {
+            self.timer?.resume()
+        }
+        self.timerStatus = .end
+        self.cancelButton.isEnabled = false //취소버튼 비활성화
+        self.setTimerInfoViewVisible(isHidden: true)
+        self.datePicker.isHidden = false
+        self.toggleButton.isSelected = false
+        self.timer?.cancel()
+        self.timer = nil //memory에서 해제
+    }
+    
+    @IBAction func tapCancelButton(_ sender: UIButton) {
+        switch self.timerStatus {
+        case .start, .pause:
+            self.stopTimer()
+        default:
+            break
+        }
+    }
+    
+    @IBAction func tapToggleButton(_ sender: UIButton) {
+        self.duration = Int(self.datePicker.countDownDuration)
+        switch self.timerStatus {
+        case .end:
+            self.currentSeconds = self.duration
+            self.timerStatus = .start
+            self.setTimerInfoViewVisible(isHidden: false)
+            self.datePicker.isHidden = true
+            self.toggleButton.isSelected = true
+            self.cancelButton.isEnabled = true
+            self.startTimer()
+            
+        case .start:
+            //타이머가 시작된 상태, 토글 버튼 타이틀이 일시정지일 때
+            self.timerStatus = .pause
+            self.toggleButton.isSelected = false
+            self.timer?.suspend() //타이머 일시정지
+        case .pause:
+            self.timerStatus = .start
+            self.toggleButton.isSelected = true
+            self.timer?.resume()
+        }
+    }
+    
 }
 
