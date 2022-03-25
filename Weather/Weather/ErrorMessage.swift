72fc284a24b73196f588eb8a161c5125a277a99d
ehvkddl <ehvkddl@naver.com>
2022-03-25 18:31:52 +0900

    Open Weather API 활용 날씨앱

---
+++ Weather/Weather/ErrorMessage.swift
@@ -0,0 +1,12 @@
+//
+//  ErrorMessage.swift
+//  Weather
+//
+//  Created by do hee kim on 2022/03/25.
+//
+
+import Foundation
+
+struct ErrorMessage: Codable {
+    let message: String
+}
---
+++ Weather/Weather/ViewController.swift
@@ -0,0 +1,69 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by do hee kim on 2022/03/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var cityNameTextField: UITextField!
+    @IBOutlet var cityNameLabel: UILabel!
+    @IBOutlet var weatherDescriptionLabel: UILabel!
+    @IBOutlet var tempLabel: UILabel!
+    @IBOutlet var maxTempLabel: UILabel!
+    @IBOutlet var minTempLabel: UILabel!
+    @IBOutlet var weatherStackView: UIStackView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func tapFetchWeatherButton(_ sender: UIButton) {
+        if let cityName = self.cityNameTextField.text {
+            self.getCurrentWeather(cityName: cityName)
+            self.view.endEditing(true) //button이 눌리면 키보드 사라지게
+        }
+    }
+    
+    func configureView(weatherInformation: WeatherInformation) {
+        self.cityNameLabel.text = weatherInformation.name
+        if let weather = weatherInformation.weather.first { //weather 배열의 첫번째 요소가 weather 상수에 대입
+            self.weatherDescriptionLabel.text = weather.description
+        }
+        self.tempLabel.text = "\(Int(weatherInformation.temp.temp - 273.15))℃"
+        self.minTempLabel.text = "최저 : \(Int(weatherInformation.temp.minTemp - 273.15))℃"
+        self.maxTempLabel.text = "최고 : \(Int(weatherInformation.temp.maxTemp - 273.15))℃"
+    }
+    
+    func showAlert(message: String) {
+        let alert = UIAlertController(title: "error", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func getCurrentWeather(cityName: String) {
+        guard let url = URL(string: "http://api.openweathermap.org/data/2.5/weather?q=\(cityName)&appid=e8a47957773ab17510637840136b6946") else { return } //optional type으로 반환되므로 옵셔널 바인딩
+        let session = URLSession(configuration: .default)
+        session.dataTask(with: url) { [weak self] data, response, error in
+            let successRange = (200..<300)
+            guard let data = data, error == nil else { return }
+            let decoder = JSONDecoder()
+            if let response = response as? HTTPURLResponse, successRange.contains(response.statusCode) { //statusCode가 200번대일때
+                guard let weatherInformation = try? decoder.decode(WeatherInformation.self, from: data) else { return }
+                DispatchQueue.main.async {
+                    self?.weatherStackView.isHidden = false
+                    self?.configureView(weatherInformation: weatherInformation)
+                }
+            } else {
+                guard let errorMessage = try? decoder.decode(ErrorMessage.self, from: data) else { return } //server에서 응답받은 error JSON data를 ErrorMessage 객체로 디코딩
+                DispatchQueue.main.async {
+                    self?.showAlert(message: errorMessage.message)
+                }
+            }
+        }.resume()
+    }
+}
+
