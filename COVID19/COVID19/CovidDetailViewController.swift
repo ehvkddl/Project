389f1f6aa3199bc4f6ce76325254ab30a3fee236
ehvkddl <ehvkddl@naver.com>
2022-03-25 22:42:53 +0900

    코로나 현황 앱 UI

---
+++ COVID19/COVID19/CovidDetailViewController.swift
@@ -0,0 +1,27 @@
+//
+//  CovidDetailViewController.swift
+//  COVID19
+//
+//  Created by do hee kim on 2022/03/25.
+//
+
+import UIKit
+
+class CovidDetailViewController: UITableViewController {
+
+    @IBOutlet var newCaseCell: UITableViewCell!
+    @IBOutlet var totalCaseCell: UITableViewCell!
+    @IBOutlet var recoveredCell: UITableViewCell!
+    @IBOutlet var deathCell: UITableViewCell!
+    @IBOutlet var percentageCell: UITableViewCell!
+    @IBOutlet var overseasInflowCell: UITableViewCell!
+    @IBOutlet var regionalOutbreakCell: UITableViewCell!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
---
+++ COVID19/COVID19/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  COVID19
+//
+//  Created by do hee kim on 2022/03/25.
+//
+
+import UIKit
+import Charts
+
+class ViewController: UIViewController {
+
+    @IBOutlet var totalCaseLabel: UILabel!
+    @IBOutlet var newCaseLabel: UILabel!
+    @IBOutlet var pieChartView: PieChartView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
