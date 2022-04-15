92d039044a51d8b700d2e058e5e48cc3c15bfcd6
ehvkddl <ehvkddl@naver.com>
2022-04-15 23:58:02 +0900

    물마시기 알람 앱 UI 구성

---
+++ Drink/Drink/AddAlertViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AddAlertViewController.swift
+//  Drink
+//
+//  Created by do hee kim on 2022/04/15.
+//
+
+import UIKit
+
+class AddAlertViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+}
