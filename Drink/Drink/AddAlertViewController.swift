97ccdc6d3775f2500c8367bce98d355c0cbbe61c
ehvkddl <ehvkddl@naver.com>
2022-04-16 01:53:07 +0900

    물마시기 알람 앱 UserDefaults

--- Drink/Drink/AddAlertViewController.swift
+++ Drink/Drink/AddAlertViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class AddAlertViewController: UIViewController {
 
+    var pickedDate: ((_ date: Date) -> Void)?
+    
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         
     }
 
-
+    @IBAction func dismissButtonAction(_ sender: UIBarButtonItem) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    @IBAction func saveButtonTapped(_ sender: UIBarButtonItem) {
+        //사람들이 선택한 datePicker에 있는 시간 부모뷰에 넘겨줘야 함
+        pickedDate?(datePicker.date)
+        self.dismiss(animated: true, completion: nil)
+    }
+    
 }
--- Drink/Drink/AlertListViewController.swift
+++ Drink/Drink/AlertListViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class AlertListViewController: UITableViewController {
-    var alertList: [Alert] = []
+    var alerts: [Alert] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,14 +17,47 @@ class AlertListViewController: UITableViewController {
         tableView.register(nibName, forCellReuseIdentifier: "AlertListCell")
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        //userDefaults에 저장된 값이 배열에 들어옴
+        alerts = alertList()
+    }
+    
     @IBAction func addAlertButtonAction(_ sender: Any) {
+        guard let addAlertVC = storyboard?.instantiateViewController(withIdentifier: "AddAlertViewController") as? AddAlertViewController else { return }
+        
+        addAlertVC.pickedDate = {[weak self] date in
+            guard let self = self else { return }
+            
+            //새로운 알럿이 생성될 때마다 테이블 뷰에 반영하는 것 뿐만 아니라 userDefaults에도 저장을 해줘야 함
+            var alertList = self.alertList() //userDefaults에서 가져온 list
+            let newAlert = Alert(date: date, isOn: true)
+            
+            alertList.append(newAlert)
+            alertList.sort { $0.date < $1.date } //시간 순서대로 정렬
+            
+            self.alerts = alertList //tableView가 바라보는 배열은 새로운 알럿을 append 한 최종 list
+            //인코딩 진행해서 userDefaults에 추가된 알럿을 넣어줌
+            UserDefaults.standard.set(try? PropertyListEncoder().encode(self.alerts), forKey: "alerts")
+            self.tableView.reloadData()
+            
+        }
+        
+        self.present(addAlertVC, animated: true, completion: nil)
+    }
+    
+    func alertList() -> [Alert] {
+        guard let data = UserDefaults.standard.value(forKey: "alerts") as? Data,
+              let alerts = try? PropertyListDecoder().decode([Alert].self, from: data) else { return [] }
+        return alerts
     }
 }
 
 //UITableView DataSource, Delegate
 extension AlertListViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return alertList.count
+        return alerts.count
     }
     
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
@@ -39,9 +72,11 @@ extension AlertListViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "AlertListCell", for: indexPath) as? AlertListCell else { return UITableViewCell() }
         
-        cell.alertSwitch.isOn = alertList[indexPath.row].isOn
-        cell.timeLabel.text = alertList[indexPath.row].time
-        cell.meridiemLabel.text = alertList[indexPath.row].meridiem
+        cell.alertSwitch.isOn = alerts[indexPath.row].isOn
+        cell.timeLabel.text = alerts[indexPath.row].time
+        cell.meridiemLabel.text = alerts[indexPath.row].meridiem
+        
+        cell.alertSwitch.tag = indexPath.row
         
         return cell
     }
@@ -58,7 +93,9 @@ extension AlertListViewController {
         switch editingStyle {
         case .delete:
             //노티피케이션 삭제 구현
-            
+            self.alerts.remove(at: indexPath.row)
+            UserDefaults.standard.set(try? PropertyListEncoder().encode(self.alerts), forKey: "alerts")
+            self.tableView.reloadData()
             return
         default:
             break
